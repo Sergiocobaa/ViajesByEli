c1c0953290070585dc268cef59a7fa2ecfbfb1c7
scobaa <scoba@tecniloop.es>
2025-10-22 17:36:00 +0200

    update colors

--- viajes-by-eli/tailwind.config.js
+++ viajes-by-eli/tailwind.config.js
@@ -4,8 +4,11 @@ export default {
   theme: {
     extend: {
       colors: {
-        eliBlue: "#33afbe",
-        eliCoral: "#fb866f",
+        // Redefine tus colores personalizados aquí
+        eliCoral: '#1D4ED8', // Ahora es un Azul más oscuro (ej. blue-700)
+        eliBlue: '#1E293B',  // Ahora es un color Gris oscuro (ej. slate-900)
+        // Puedes añadir más si lo necesitas
+        eliSuccess: '#10B981', // emerald-500
       },
       animation: {
         fadeIn: "fadeIn 0.3s ease-in-out",
